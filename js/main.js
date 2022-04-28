f0e7cd76a3a0c54b7cbc37e5978c6fcdd324e722
unknown <singilthor@gmail.com>
2022-04-28 11:21:33 +0600

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// arr.forEach((item, index, array) => {
+//     console.log(item * 2);
+// }
+// )
+
+// let newArr = arr.map((item) => {
+//     return item + 10 
+// })
+// console.log(newArr);
+
+// arr.map((item, index, array) => {
+//     console.log
+
+
+// })
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let newArr = arr.filter((item, index, array) => {
+
+//     return item > 4
+// })
+// console.log(newArr);
+
+let str = 'Ivan, Misha, Abdrei, Oleg, Aktan';
+let filteredArr = str.split(', ').filter(item=> {
+
+    
+})
